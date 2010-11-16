a15e33a772b94869ed02252b0fb406b8599740a9
Amancio Díaz <amancio.diaz@gmail.com>
2010-11-16 14:17:24 +0100

    Backport to old version with other functionality for client use only. Not tested as XMLRPC server.

--- src/xmlrpc.erl
+++ src/xmlrpc.erl
@@ -1,12 +1,12 @@
-%% Copyright (C) 2003 Joakim Grebenö <jocke@gleipnir.com>.
+%% Copyright (C) 2003 Joakim Grebenï¿½ <jocke@gleipnir.com>.
 %% All rights reserved.
 %%
 %% Redistribution and use in source and binary forms, with or without
 %% modification, are permitted provided that the following conditions
-%% are met: 
+%% are met:
 %%
 %% 1. Redistributions of source code must retain the above copyright
-%%    notice, this list of conditions and the following disclaimer. 
+%%    notice, this list of conditions and the following disclaimer.
 %% 2. Redistributions in binary form must reproduce the above
 %%    copyright notice, this list of conditions and the following
 %%    disclaimer in the documentation and/or other materials provided
@@ -29,6 +29,15 @@
 -export([call/3, call/4, call/5, call/6]).
 -export([start_link/1, start_link/5, start_link/6, stop/1]).
 
+-export([ssl_call/3, ssl_call/4, ssl_call/5, ssl_call/6]).
+
+-export([cbs_new/0, cbs_record/1,
+	 cbs_ip/1, cbs_ip/2,
+	 cbs_port/1, cbs_port/2,
+	 cbs_opaque/1, cbs_opaque/2
+	]).
+
+
 -include("log.hrl").
 
 -record(header, {
@@ -38,13 +47,68 @@
 	  connection
 	 }).
 
+%%% This record can be used by the client to be able to
+%%% retrieve internal info (such as Ip,Port) and at the
+%%% same time preserve the clients own state (opaque).
+%%% NB: No include file is provided, use the access functions!
+-record(cback_state, {
+	  ip,
+	  port,
+	  opaque
+	 }).
+
+cbs_record(C) when is_record(C, cback_state) -> true;
+cbs_record(_)                             -> false.
+
+cbs_new() ->
+    #cback_state{}.
+
+cbs_ip(C) when is_record(C, cback_state) ->
+    C#cback_state.ip.
+cbs_ip(C, Ip) when is_record(C, cback_state) ->
+    C#cback_state{ip = Ip}.
+
+cbs_port(C) when is_record(C, cback_state) ->
+    C#cback_state.port.
+cbs_port(C, Port) when is_record(C, cback_state) ->
+    C#cback_state{port = Port}.
+
+cbs_opaque(C) when is_record(C, cback_state) ->
+    C#cback_state.opaque.
+cbs_opaque(C, Opaque) when is_record(C, cback_state) ->
+    C#cback_state{opaque = Opaque}.
+
+
+%%%
+%%% Quick and dirty solution for adding SSL support.
+%%%
+-define(SSL, ssl).
+
+ssl_call(Host, Port, URI) ->
+    put(proto, ?SSL),
+    call(Host, Port, URI).
+
+ssl_call(Host, Port, URI, Payload) ->
+    put(proto, ?SSL),
+    call(Host, Port, URI, Payload).
+
+ssl_call(Socket, URI, Payload, KeepAlive, Timeout) ->
+    put(proto, ?SSL),
+    call(Socket, URI, Payload, KeepAlive, Timeout).
+
+ssl_call(Host, Port, URI, Payload, KeepAlive, Timeout) ->
+    put(proto, ?SSL),
+    call(Host, Port, URI, Payload, KeepAlive, Timeout).
+
+
 %% Exported: call/{3,4,5,6}
 
-call(Host, Port, URI, Payload) -> call(Host, Port, URI, Payload, false, 60000).
+call(Host, Port, URI, Payload) ->
+    call(Host, Port, URI, Payload, false, 60000).
 
 call(Host, Port, URI, Payload, KeepAlive, Timeout) ->
-    case gen_tcp:connect(Host, Port, [{active, false}]) of
-	{ok, Socket} -> call(Socket, URI, Payload, KeepAlive, Timeout);
+    case connect(Host, Port, [{active, false}]) of
+	{ok, Socket} -> call(Socket, {Host,URI}, Payload, KeepAlive, Timeout);
 	{error, Reason} when KeepAlive == false -> {error, Reason};
 	{error, Reason} -> {error, undefined, Reason}
     end.
@@ -62,39 +126,50 @@ call(Socket, URI, Payload, KeepAlive, Timeout) ->
 			{ok, Header} ->
 			    handle_payload(Socket, KeepAlive, Timeout, Header);
 			{error, Reason} when KeepAlive == false ->
-			    gen_tcp:close(Socket),
+			    close(Socket),
 			    {error, Reason};
 			{error, Reason} -> {error, Socket, Reason}
 		    end;
 		{error, Reason} when KeepAlive == false ->
-		    gen_tcp:close(Socket),
+		    close(Socket),
 		    {error, Reason};
-		{error, Reason} -> {error, Socket, Reason}
+		{error, Reason} ->
+		    {error, Socket, Reason}
 	    end;
 	{error, Reason} when KeepAlive == false ->
-	    gen_tcp:close(Socket),
+	    close(Socket),
 	    {error, Reason};
 	{error, Reason} -> {error, Socket, Reason}
     end.
 
 send(Socket, URI, false, Payload) ->
     send(Socket, URI, "Connection: close\r\n", Payload);
-send(Socket, URI, true, Payload) -> send(Socket, URI, "Connection: keep-alive\r\n", Payload);
-send(Socket, URI, Header, Payload) ->
+send(Socket, URI, true, Payload) -> send(Socket, URI, "", Payload);
+send(Socket, {Host,URI}, Header, Payload) ->
     Request =
 	["POST ", URI, " HTTP/1.1\r\n",
 	 "Content-Length: ", integer_to_list(lists:flatlength(Payload)),
 	 "\r\n",
 	 "User-Agent: Erlang XML-RPC Client 1.13\r\n",
 	 "Content-Type: text/xml\r\n",
+	 "Host: ", Host, "\r\n",
 	 Header, "\r\n",
 	 Payload],
-    gen_tcp:send(Socket, Request).
+    send(Socket, Request);
+send(Socket, URI, Header, Payload) ->
+    Request =
+	["POST ", URI, " HTTP/1.1\r\n",
+	 "Content-Length: ", integer_to_list(lists:flatlength(Payload)),
+	 "\r\n",
+	 "User-Agent: Erlang XML-RPC Client 1.13\r\n",
+	 "Content-Type: text/XML\r\n",
+	 Header, "\r\n",
+	 Payload],
+    send(Socket, Request).
 
 parse_response(Socket, Timeout) ->
-    inet:setopts(Socket, [{packet, line}]),
-    case gen_tcp:recv(Socket, 0, Timeout) of
-	{ok, "HTTP/1.0 200 OK\r\n"} -> parse_header(Socket, Timeout);
+    setopts(Socket, [{packet, line}]),
+    case recv(Socket, 0, Timeout) of
 	{ok, "HTTP/1.1 200 OK\r\n"} -> parse_header(Socket, Timeout);
 	{ok, StatusLine} -> {error, StatusLine};
 	{error, Reason} -> {error, Reason}
@@ -103,21 +178,21 @@ parse_response(Socket, Timeout) ->
 parse_header(Socket, Timeout) -> parse_header(Socket, Timeout, #header{}).
 
 parse_header(Socket, Timeout, Header) ->
-	case gen_tcp:recv(Socket, 0, Timeout) of
+    case recv(Socket, 0, Timeout) of
 	{ok, "\r\n"} when Header#header.content_length == undefined ->
 	    {error, missing_content_length};
 	{ok, "\r\n"} -> {ok, Header};
 	{ok, HeaderField} ->
-	    case string:tokens(string:to_lower(HeaderField), " \r\n") of
-		["content-length:", ContentLength] ->
-			try
-				Value = list_to_integer(ContentLength),
+	    case string:tokens(HeaderField, " \r\n") of
+		["Content-Length:", ContentLength] ->
+		    case catch list_to_integer(ContentLength) of
+			Value when is_integer(Value) ->
 			    parse_header(Socket, Timeout,
-					 Header#header{content_length = Value})
-		    catch
-				_ -> {error, {invalid_content_length, ContentLength}}
+					 Header#header{content_length =
+						       Value});
+			_ -> {error, {invalid_content_length, ContentLength}}
 		    end;
-		["connection:", "close"] ->
+		["Connection:", "close"] ->
 		    parse_header(Socket, Timeout,
 				 Header#header{connection = close});
 		_ ->
@@ -133,51 +208,52 @@ handle_payload(Socket, KeepAlive, Timeout, Header) ->
 	    case xmlrpc_decode:payload(Payload) of
 		{ok, DecodedPayload} when KeepAlive == false ->
 		    ?DEBUG_LOG({decoded_response, DecodedPayload}),
-		    gen_tcp:close(Socket),
+		    close(Socket),
 		    {ok, DecodedPayload};
 		{ok, DecodedPayload} when KeepAlive == true,
 					  Header#header.connection == close ->
 		    ?DEBUG_LOG({decoded_response, DecodedPayload}),
-		    gen_tcp:close(Socket),
+		    close(Socket),
 		    {ok, Socket, DecodedPayload};
 		{ok, DecodedPayload} ->
 		    ?DEBUG_LOG({decoded_response, DecodedPayload}),
 		    {ok, Socket, DecodedPayload};
 		{error, Reason} when KeepAlive == false ->
-		    gen_tcp:close(Socket),
+		    close(Socket),
 		    {error, Reason};
 		{error, Reason} when KeepAlive == true,
 				     Header#header.connection == close ->
-		    gen_tcp:close(Socket),
+		    close(Socket),
 		    {error, Socket, Reason};
 		{error, Reason} ->
 		    {error, Socket, Reason}
 	    end;
 	{error, Reason} when KeepAlive == false ->
-	    gen_tcp:close(Socket),
+	    close(Socket),
 	    {error, Reason};
 	{error, Reason} when KeepAlive == true,
 			     Header#header.connection == close ->
-	    gen_tcp:close(Socket),
+	    close(Socket),
 	    {error, Socket, Reason};
 	{error, Reason} -> {error, Socket, Reason}
     end.
 
 get_payload(Socket, Timeout, ContentLength) ->
-    inet:setopts(Socket, [{packet, raw}]),
-    gen_tcp:recv(Socket, ContentLength, Timeout).
+    setopts(Socket, [{packet, raw}]),
+    recv(Socket, ContentLength, Timeout).
 
 %% Exported: start_link/{1,5,6}
 
-start_link(Handler) -> start_link(4567, 1000, 60000, Handler, undefined).
+start_link(Handler) ->
+    start_link(4567, 1000, 60000, Handler, undefined).
 
 start_link(Port, MaxSessions, Timeout, Handler, State) ->
     start_link(all, Port, MaxSessions, Timeout, Handler, State).
 
 start_link(IP, Port, MaxSessions, Timeout, Handler, State) ->
     OptionList = [{active, false}, {reuseaddr, true}|ip(IP)],
-    SessionHandler = {xmlrpc_http, handler, [Timeout, Handler, State]}, 
-    xmlrpc_tcp_serv:start_link([Port, MaxSessions, OptionList, SessionHandler]).
+    SessionHandler = {xmlrpc_http, handler, [Timeout, Handler, State]},
+    tcp_serv:start_link([Port, MaxSessions, OptionList, SessionHandler]).
 
 ip(all) -> [];
 ip(IP) when is_tuple(IP) -> {ip, IP}.
@@ -185,3 +261,46 @@ ip(IP) when is_tuple(IP) -> {ip, IP}.
 %% Exported: stop/1
 
 stop(Pid) -> tcp_serv:stop(Pid).
+
+
+%%%
+%%% Switch on bearer protocol to be used
+%%%
+connect(Host, Port, Opts) ->
+    connect(get(proto), Host, Port, Opts).
+
+connect(?SSL, Host, Port, Opts) -> ssl:connect(Host, Port, Opts);
+connect(_,    Host, Port, Opts) -> gen_tcp:connect(Host, Port, Opts).
+
+
+close(Socket) ->
+    close(get(proto), Socket).
+
+close(?SSL, Socket) -> ssl:close(Socket);
+close(_   , Socket) -> gen_tcp:close(Socket).
+
+
+send(Socket, Request) ->
+    send(get(proto), Socket, Request).
+
+send(?SSL, Socket, Request) -> ssl:send(Socket, Request);
+send(_   , Socket, Request) -> gen_tcp:send(Socket, Request).
+
+
+recv(Socket, Length, Timeout) ->
+    recv(get(proto), Socket, Length, Timeout).
+
+recv(?SSL, Socket, Length, Timeout) -> ssl:recv(Socket, Length, Timeout);
+recv(_   , Socket, Length, Timeout) -> gen_tcp:recv(Socket, Length, Timeout).
+
+
+
+setopts(Socket, Opts) ->
+    setopts(get(proto), Socket, Opts).
+
+setopts(?SSL, Socket, Opts) -> ssl:setopts(Socket, Opts);
+setopts(_,    Socket, Opts) -> inet:setopts(Socket, Opts).
+
+
+
+
